186606c06faf40da685335fc396baa98aae875d8
Travis Ralston <travpc@gmail.com>
2018-06-02 16:43:31 -0600

    Update the access control headers and CSP

--- src/github.com/turt2live/matrix-media-repo/api/webserver/route_handler.go
+++ src/github.com/turt2live/matrix-media-repo/api/webserver/route_handler.go
@@ -52,9 +52,9 @@ func (h handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 
 	// Send CORS and other basic headers
 	w.Header().Set("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization")
-	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, OPTIONS")
+	w.Header().Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
 	w.Header().Set("Access-Control-Allow-Origin", "*")
-	w.Header().Set("Content-Security-Policy", "default-src 'none'; script-src 'none'; plugin-types application/pdf; style-src 'unsafe-inline'; object-src 'self'")
+	w.Header().Set("Content-Security-Policy", "default-src 'none'; script-src 'none'; plugin-types application/pdf; style-src 'unsafe-inline'; object-src 'self';")
 	w.Header().Set("Cache-Control", "public,max-age=86400,s-maxage=86400")
 	w.Header().Set("Server", "matrix-media-repo")
 
