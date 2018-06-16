c5dabd2083c673aaed4e0e0d47d83ff53eec8a79
Travis Ralston <travpc@gmail.com>
2018-06-16 17:48:14 -0600

    Redirect uploads through the new middle layer structure
    Part of #58

--- src/github.com/turt2live/matrix-media-repo/controllers/download_controller/download_resource_handler.go
+++ src/github.com/turt2live/matrix-media-repo/controllers/download_controller/download_resource_handler.go
@@ -5,20 +5,17 @@ import (
 	"errors"
 	"io"
 	"mime"
-	"os"
 	"strconv"
 	"sync"
 	"time"
 
 	"github.com/patrickmn/go-cache"
-	"github.com/ryanuber/go-glob"
 	"github.com/sirupsen/logrus"
 	"github.com/turt2live/matrix-media-repo/common"
 	"github.com/turt2live/matrix-media-repo/common/config"
+	"github.com/turt2live/matrix-media-repo/controllers/upload_controller"
 	"github.com/turt2live/matrix-media-repo/matrix"
-	"github.com/turt2live/matrix-media-repo/storage"
 	"github.com/turt2live/matrix-media-repo/types"
-	"github.com/turt2live/matrix-media-repo/util"
 	"github.com/turt2live/matrix-media-repo/util/resource_handler"
 )
 
@@ -90,109 +87,13 @@ func downloadResourceWorkFn(request *resource_handler.WorkRequest) interface{} {
 
 	defer downloaded.Contents.Close()
 
-	media, err := StoreDirect(downloaded.Contents, downloaded.ContentType, downloaded.DesiredFilename, info.origin, info.mediaId, ctx, log)
+	userId := upload_controller.NoApplicableUploadUser
+	media, err := upload_controller.StoreDirect(downloaded.Contents, downloaded.ContentType, downloaded.DesiredFilename, userId, info.origin, info.mediaId, ctx, log)
 	if err != nil {
 		return &downloadResponse{err: err}
 	}
 
 	return &downloadResponse{media, err}
-}
-
-func StoreDirect(contents io.Reader, contentType string, filename string, origin string, mediaId string, ctx context.Context, log *logrus.Entry) (*types.Media, error) {
-	fileLocation, err := storage.PersistFile(contents, ctx, log)
-	if err != nil {
-		return nil, err
-	}
-
-	fileMime, err := util.GetMimeType(fileLocation)
-	if err != nil {
-		log.Error("Error while checking content type of file: ", err.Error())
-		os.Remove(fileLocation) // delete temp file
-		return nil, err
-	}
-
-	for _, allowedType := range config.Get().Uploads.AllowedTypes {
-		if !glob.Glob(allowedType, fileMime) {
-			log.Warn("Content type " + fileMime +" (reported as " + contentType+") is not allowed to be uploaded")
-
-			os.Remove(fileLocation) // delete temp file
-			return nil, common.ErrMediaNotAllowed
-		}
-	}
-
-	hash, err := storage.GetFileHash(fileLocation)
-	if err != nil {
-		os.Remove(fileLocation) // delete temp file
-		return nil, err
-	}
-
-	db := storage.GetDatabase().GetMediaStore(ctx, log)
-	records, err := db.GetByHash(hash)
-	if err != nil {
-		os.Remove(fileLocation) // delete temp file
-		return nil, err
-	}
-
-	if len(records) > 0 {
-		log.Info("Duplicate media for hash ", hash)
-
-		// We'll use the location from the first record
-		media := records[0]
-		media.Origin = origin
-		media.MediaId = mediaId
-		media.UserId = ""
-		media.UploadName = filename
-		media.ContentType = contentType
-		media.CreationTs = util.NowMillis()
-
-		err = db.Insert(media)
-		if err != nil {
-			os.Remove(fileLocation) // delete temp file
-			return nil, err
-		}
-
-		// If the media's file exists, we'll delete the temp file
-		// If the media's file doesn't exist, we'll move the temp file to where the media expects it to be
-		exists, err := util.FileExists(media.Location)
-		if err != nil || !exists {
-			// We'll assume an error means it doesn't exist
-			os.Rename(fileLocation, media.Location)
-		} else {
-			os.Remove(fileLocation)
-		}
-
-		return media, nil
-	}
-
-	// The media doesn't already exist - save it as new
-
-	fileSize, err := util.FileSize(fileLocation)
-	if err != nil {
-		os.Remove(fileLocation) // delete temp file
-		return nil, err
-	}
-
-	log.Info("Persisting new media record")
-
-	media := &types.Media{
-		Origin:      origin,
-		MediaId:     mediaId,
-		UploadName:  filename,
-		ContentType: contentType,
-		UserId:      "",
-		Sha256Hash:  hash,
-		SizeBytes:   fileSize,
-		Location:    fileLocation,
-		CreationTs:  util.NowMillis(),
-	}
-
-	err = db.Insert(media)
-	if err != nil {
-		os.Remove(fileLocation) // delete temp file
-		return nil, err
-	}
-
-	return media, nil
 }
 
 func DownloadRemoteMediaDirect(server string, mediaId string, log *logrus.Entry) (*downloadedMedia, error) {
---
+++ src/github.com/turt2live/matrix-media-repo/controllers/upload_controller/upload_controller.go
@@ -0,0 +1,166 @@
+package upload_controller
+
+import (
+	"context"
+	"io"
+	"os"
+	"strconv"
+
+	"github.com/ryanuber/go-glob"
+	"github.com/sirupsen/logrus"
+	"github.com/turt2live/matrix-media-repo/common"
+	"github.com/turt2live/matrix-media-repo/common/config"
+	"github.com/turt2live/matrix-media-repo/storage"
+	"github.com/turt2live/matrix-media-repo/types"
+	"github.com/turt2live/matrix-media-repo/util"
+)
+
+const NoApplicableUploadUser = ""
+
+func IsRequestTooLarge(contentLength int64, contentLengthHeader string) bool {
+	if config.Get().Uploads.MaxSizeBytes <= 0 {
+		return false
+	}
+	if contentLength >= 0 {
+		return contentLength > config.Get().Uploads.MaxSizeBytes
+	}
+	if contentLengthHeader != "" {
+		parsed, err := strconv.ParseInt(contentLengthHeader, 10, 64)
+		if err != nil {
+			logrus.Warn("Invalid content length header given; assuming too large. Value received: " + contentLengthHeader)
+			return true // Invalid header
+		}
+
+		return parsed > config.Get().Uploads.MaxSizeBytes
+	}
+
+	return false // We can only assume
+}
+
+func UploadMedia(contents io.ReadCloser, contentType string, filename string, userId string, origin string, ctx context.Context, log *logrus.Entry) (*types.Media, error) {
+	defer contents.Close()
+
+	var data io.Reader
+	if config.Get().Uploads.MaxSizeBytes > 0 {
+		data = io.LimitReader(contents, config.Get().Uploads.MaxSizeBytes)
+	} else {
+		data = contents
+	}
+
+	mediaId, err := util.GenerateRandomString(64)
+	if err != nil {
+		return nil, err
+	}
+
+	return StoreDirect(data, contentType, filename, userId, origin, mediaId, ctx, log)
+}
+
+func StoreDirect(contents io.Reader, contentType string, filename string, userId string, origin string, mediaId string, ctx context.Context, log *logrus.Entry) (*types.Media, error) {
+	fileLocation, err := storage.PersistFile(contents, ctx, log)
+	if err != nil {
+		return nil, err
+	}
+
+	fileMime, err := util.GetMimeType(fileLocation)
+	if err != nil {
+		log.Error("Error while checking content type of file: ", err.Error())
+		os.Remove(fileLocation) // delete temp file
+		return nil, err
+	}
+
+	for _, allowedType := range config.Get().Uploads.AllowedTypes {
+		if !glob.Glob(allowedType, fileMime) {
+			log.Warn("Content type " + fileMime +" (reported as " + contentType+") is not allowed to be uploaded")
+
+			os.Remove(fileLocation) // delete temp file
+			return nil, common.ErrMediaNotAllowed
+		}
+	}
+
+	hash, err := storage.GetFileHash(fileLocation)
+	if err != nil {
+		os.Remove(fileLocation) // delete temp file
+		return nil, err
+	}
+
+	db := storage.GetDatabase().GetMediaStore(ctx, log)
+	records, err := db.GetByHash(hash)
+	if err != nil {
+		os.Remove(fileLocation) // delete temp file
+		return nil, err
+	}
+
+	if len(records) > 0 {
+		log.Info("Duplicate media for hash ", hash)
+
+		// If the user is a real user (ie: actually uploaded media), then we'll see if there's
+		// an exact duplicate that we can return. Otherwise we'll just pick the first record and
+		// clone that.
+		if userId != NoApplicableUploadUser {
+			for _, record := range records {
+				if record.UserId == userId && record.Origin == origin && record.ContentType == contentType {
+					log.Info("User has already uploaded this media before - returning unaltered media record")
+					os.Remove(fileLocation) // delete temp file
+					return record, nil
+				}
+			}
+		}
+
+		// We'll use the location from the first record
+		media := records[0]
+		media.Origin = origin
+		media.MediaId = mediaId
+		media.UserId = userId
+		media.UploadName = filename
+		media.ContentType = contentType
+		media.CreationTs = util.NowMillis()
+
+		err = db.Insert(media)
+		if err != nil {
+			os.Remove(fileLocation) // delete temp file
+			return nil, err
+		}
+
+		// If the media's file exists, we'll delete the temp file
+		// If the media's file doesn't exist, we'll move the temp file to where the media expects it to be
+		exists, err := util.FileExists(media.Location)
+		if err != nil || !exists {
+			// We'll assume an error means it doesn't exist
+			os.Rename(fileLocation, media.Location)
+		} else {
+			os.Remove(fileLocation)
+		}
+
+		return media, nil
+	}
+
+	// The media doesn't already exist - save it as new
+
+	fileSize, err := util.FileSize(fileLocation)
+	if err != nil {
+		os.Remove(fileLocation) // delete temp file
+		return nil, err
+	}
+
+	log.Info("Persisting new media record")
+
+	media := &types.Media{
+		Origin:      origin,
+		MediaId:     mediaId,
+		UploadName:  filename,
+		ContentType: contentType,
+		UserId:      userId,
+		Sha256Hash:  hash,
+		SizeBytes:   fileSize,
+		Location:    fileLocation,
+		CreationTs:  util.NowMillis(),
+	}
+
+	err = db.Insert(media)
+	if err != nil {
+		os.Remove(fileLocation) // delete temp file
+		return nil, err
+	}
+
+	return media, nil
+}
