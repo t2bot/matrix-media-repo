0919f2f9c534d294ae795ccb138c3fd0dc7344d2
Travis Ralston <travpc@gmail.com>
2019-04-16 10:48:47 -0600

    Support storagePaths configuration option at a higher level

--- src/github.com/turt2live/matrix-media-repo/storage/datastore/datastore.go
+++ src/github.com/turt2live/matrix-media-repo/storage/datastore/datastore.go
@@ -66,62 +66,6 @@ func GetUriForDatastore(dsConf config.DatastoreConfig) string {
 }
 
 func PickDatastore(ctx context.Context, log *logrus.Entry) (*DatastoreRef, error) {
-	// Legacy options first
-	storagePaths := config.Get().Uploads.StoragePaths
-	if len(storagePaths) > 0 {
-		log.Warn("Using legacy options to find a datastore")
-
-		if len(storagePaths) == 1 {
-			ds, err := storage.GetOrCreateDatastoreOfType(ctx, log, "file", storagePaths[0])
-			if err != nil {
-				return nil, err
-			}
-
-			fakeConfig := config.DatastoreConfig{
-				Type:       "file",
-				Enabled:    true,
-				ForUploads: true,
-				Options:    map[string]string{"path": ds.Uri},
-			}
-			return newDatastoreRef(ds, fakeConfig), nil
-		}
-
-		var basePath string
-		var pathSize int64
-		for i := 0; i < len(storagePaths); i++ {
-			currPath := storagePaths[i]
-			ds, err := storage.GetOrCreateDatastoreOfType(ctx, log, "file", currPath)
-			if err != nil {
-				continue
-			}
-
-			size, err := estimatedDatastoreSize(ds, ctx, log)
-			if err != nil {
-				continue
-			}
-
-			if basePath == "" || size < pathSize {
-				basePath = currPath
-				pathSize = size
-			}
-		}
-
-		if basePath != "" {
-			ds, err := storage.GetOrCreateDatastoreOfType(ctx, log, "file", basePath)
-			if err != nil {
-				return nil, err
-			}
-
-			fakeConfig := config.DatastoreConfig{
-				Type:       "file",
-				Enabled:    true,
-				ForUploads: true,
-				Options:    map[string]string{"path": ds.Uri},
-			}
-			return newDatastoreRef(ds, fakeConfig), nil
-		}
-	}
-
 	// If we haven't found a legacy option, pick a datastore
 	log.Info("Finding a suitable datastore to pick for uploads")
 	confDatastores := config.Get().DataStores
