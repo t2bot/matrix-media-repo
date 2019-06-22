0aa84a839dac2dc3886639e416db131d057af4a6
Travis Ralston <travpc@gmail.com>
2019-06-22 12:33:23 -0600

    Proof of concept for request locking on thumbnails
    Untested: whether the thumbnails come out okay when using a shared reader.

--- api/r0/thumbnail.go
+++ api/r0/thumbnail.go
@@ -1,6 +1,7 @@
 package r0
 
 import (
+	"fmt"
 	"net/http"
 	"strconv"
 
@@ -10,7 +11,10 @@ import (
 	"github.com/turt2live/matrix-media-repo/common"
 	"github.com/turt2live/matrix-media-repo/common/config"
 	"github.com/turt2live/matrix-media-repo/controllers/thumbnail_controller"
+	"golang.org/x/sync/singleflight"
 )
+
+var thumbnailRequestGroup singleflight.Group
 
 func ThumbnailMedia(r *http.Request, log *logrus.Entry, user api.UserInfo) interface{} {
 	params := mux.Vars(r)
@@ -75,21 +79,36 @@ func ThumbnailMedia(r *http.Request, log *logrus.Entry, user api.UserInfo) inter
 		"requestedAnimated": animated,
 	})
 
-	streamedThumbnail, err := thumbnail_controller.GetThumbnail(server, mediaId, width, height, animated, method, downloadRemote, r.Context(), log)
-	if err != nil {
-		if err == common.ErrMediaNotFound {
-			return api.NotFoundError()
-		} else if err == common.ErrMediaTooLarge {
-			return api.RequestTooLarge()
+	// TODO: Move this to a lower layer (somewhere where the thumbnail dimensions are known, before media is downloaded)
+	requestKey := fmt.Sprintf("thumbnail_%s_%s_%d_%d_%s_%t", server, mediaId, width, height, method, animated)
+	v, err, shared := thumbnailRequestGroup.Do(requestKey, func() (interface{}, error) {
+		streamedThumbnail, err := thumbnail_controller.GetThumbnail(server, mediaId, width, height, animated, method, downloadRemote, r.Context(), log)
+		if err != nil {
+			if err == common.ErrMediaNotFound {
+				return api.NotFoundError(), nil
+			} else if err == common.ErrMediaTooLarge {
+				return api.RequestTooLarge(), nil
+			}
+			log.Error("Unexpected error locating media: " + err.Error())
+			return api.InternalServerError("Unexpected Error"), nil
 		}
-		log.Error("Unexpected error locating media: " + err.Error())
+
+		return &DownloadMediaResponse{
+			ContentType: streamedThumbnail.Thumbnail.ContentType,
+			SizeBytes:   streamedThumbnail.Thumbnail.SizeBytes,
+			Data:        streamedThumbnail.Stream,
+			Filename:    "thumbnail",
+		}, nil
+	})
+
+	if err != nil {
+		log.Error("Unexpected error handling request: " + err.Error())
 		return api.InternalServerError("Unexpected Error")
 	}
 
-	return &DownloadMediaResponse{
-		ContentType: streamedThumbnail.Thumbnail.ContentType,
-		SizeBytes:   streamedThumbnail.Thumbnail.SizeBytes,
-		Data:        streamedThumbnail.Stream,
-		Filename:    "thumbnail",
+	if shared {
+		log.Info("Request response was shared")
 	}
+
+	return v
 }
