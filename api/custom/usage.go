cfd20e3ddb21d6f06aa277a49fdc694bd9f0d5d3
Travis Ralston <travpc@gmail.com>
2019-07-31 15:32:46 -0600

    Add early stats for the per-domain level
    Fixes https://github.com/turt2live/matrix-media-repo/issues/169

---
+++ api/custom/usage.go
@@ -0,0 +1,70 @@
+package custom
+
+import (
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
+	"github.com/turt2live/matrix-media-repo/api"
+	"github.com/turt2live/matrix-media-repo/storage"
+)
+
+type UsageInfo struct {
+	Total      int64 `json:"total"`
+	Media      int64 `json:"media"`
+	Thumbnails int64 `json:"thumbnails"`
+}
+
+type DomainUsageResponse struct {
+	NumUsers  int       `json:"user_count"`
+	UserIDs   []string  `json:"known_user_ids"`
+	RawBytes  UsageInfo `json:"raw_bytes"`
+	RawCounts UsageInfo `json:"raw_counts"`
+}
+
+func GetDomainUsage(r *http.Request, log *logrus.Entry, user api.UserInfo) interface{} {
+	// TODO: Auth check on host (allow local admin to query their own domain)
+
+	params := mux.Vars(r)
+
+	serverName := params["serverName"]
+
+	log = log.WithFields(logrus.Fields{
+		"serverName": serverName,
+	})
+
+	db := storage.GetDatabase().GetMetadataStore(r.Context(), log)
+
+	userIds, err := db.GetUsersForServer(serverName)
+	if err != nil {
+		log.Error(err)
+		return api.InternalServerError("Failed to get users belonging to server")
+	}
+
+	mediaBytes, thumbBytes, err := db.GetByteUsageForServer(serverName)
+	if err != nil {
+		log.Error(err)
+		return api.InternalServerError("Failed to get byte usage for server")
+	}
+
+	mediaCount, thumbCount, err := db.GetCountUsageForServer(serverName)
+	if err != nil {
+		log.Error(err)
+		return api.InternalServerError("Failed to get count usage for server")
+	}
+
+	return &DomainUsageResponse{
+		NumUsers: len(userIds),
+		UserIDs:  userIds,
+		RawBytes: UsageInfo{
+			Total:      mediaBytes + thumbBytes,
+			Media:      mediaBytes,
+			Thumbnails: thumbBytes,
+		},
+		RawCounts: UsageInfo{
+			Total:      mediaCount + thumbCount,
+			Media:      mediaCount,
+			Thumbnails: thumbCount,
+		},
+	}
+}
---
+++ migrations/9_origin_indexes_down.sql
@@ -0,0 +1,3 @@
+DROP INDEX IF EXISTS idx_origin_media;
+DROP INDEX IF EXISTS idx_origin_thumbnails;
+DROP INDEX IF EXISTS idx_origin_user_id_media;
