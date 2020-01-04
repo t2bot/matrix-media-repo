2bd2bc457f5e36790a7bffaeae4cb2d1d7816c77
Travis Ralston <travpc@gmail.com>
2020-01-04 16:34:07 -0700

    Fix build scripts

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 GOBIN=$PWD/bin go install -v ./cmd/compile_assets
-compile_assets
+$PWD/bin/compile_assets
 GOBIN=$PWD/bin go install -ldflags "-X github.com/turt2live/matrix-media-repo/common/version.GitCommit=$(git rev-list -1 HEAD) -X github.com/turt2live/matrix-media-repo/common/version.Version=$(git describe --tags)" -v ./cmd/...
