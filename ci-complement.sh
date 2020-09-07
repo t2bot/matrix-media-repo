72a5fff06278a157da9355f5b0e48a87f1c832f1
Travis Ralston <travpc@gmail.com>
2020-09-06 22:29:50 -0600

    Update complement CI

--- ci-complement.sh
+++ ci-complement.sh
@@ -2,6 +2,4 @@
 
 git clone --depth=1 https://github.com/matrix-org/complement.git CI_COMPLEMENT
 cd CI_COMPLEMENT
-git fetch origin pull/14/head:complement-with-timeout
-git checkout complement-with-timeout
 go test -run '^(TestMediaWithoutFileName)$' -v ./tests
