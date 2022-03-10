b57f092d14019394c92cd1f4f0b7bd8e984d2b2d
mattc <buffless-matt@users.noreply.github.com>
2022-03-10 16:28:13 +1100

    Add - parameterized admin API endpoint to GET users' usage statistics for a server.

--- api/custom/usage.go
+++ api/custom/usage.go
@@ -1,14 +1,19 @@
 package custom
 
 import (
+	"encoding/json"
+	"fmt"
 	"github.com/getsentry/sentry-go"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 	"github.com/sirupsen/logrus"
 	"github.com/turt2live/matrix-media-repo/api"
 	"github.com/turt2live/matrix-media-repo/common/rcontext"
 	"github.com/turt2live/matrix-media-repo/storage"
+	"github.com/turt2live/matrix-media-repo/storage/stores"
 	"github.com/turt2live/matrix-media-repo/types"
 	"github.com/turt2live/matrix-media-repo/util"
 )
@@ -206,4 +211,127 @@ func GetUploadsUsage(r *http.Request, rctx rcontext.RequestContext, user api.Use
 	}
 
 	return &api.DoNotCacheResponse{Payload: parsed}
+}
+
+// GetUsersUsageStats attempts to provide a loose equivalent to this Synapse admin end-point:
+// https://matrix-org.github.io/synapse/develop/admin_api/statistics.html#users-media-usage-statistics
+func GetUsersUsageStats(r *http.Request, rctx rcontext.RequestContext, _ api.UserInfo) interface{} {
+	params := mux.Vars(r)
+	qs := r.URL.Query()
+	var err error
+
+	serverName := params["serverName"]
+
+	orderBy := qs.Get("order_by")
+	if len(qs["order_by"]) == 0 {
+		orderBy = "user_id"
+	}
+	if !util.ArrayContains(stores.UsersUsageStatsSorts, orderBy) {
+		acceptedValsStr, _ := json.Marshal(stores.UsersUsageStatsSorts)
+		acceptedValsStr = []byte(strings.ReplaceAll(string(acceptedValsStr), "\"", "'"))
+		return api.BadRequest(
+			fmt.Sprintf("Query parameter 'order_by' must be one of %s", acceptedValsStr))
+	}
+
+	var start int64 = 0
+	if len(qs["from"]) > 0 {
+		start, err = strconv.ParseInt(qs.Get("from"), 10, 64)
+		if err != nil || start < 0 {
+			return api.BadRequest("Query parameter 'from' must be a non-negative integer")
+		}
+	}
+
+	var limit int64 = 100
+	if len(qs["limit"]) > 0 {
+		limit, err = strconv.ParseInt(qs.Get("limit"), 10, 64)
+		if err != nil || limit < 0 {
+			return api.BadRequest("Query parameter 'limit' must be a non-negative integer")
+		}
+	}
+
+	const unspecifiedTS int64 = -1
+	fromTS := unspecifiedTS
+	if len(qs["from_ts"]) > 0 {
+		fromTS, err = strconv.ParseInt(qs.Get("from_ts"), 10, 64)
+		if err != nil || fromTS < 0 {
+			return api.BadRequest("Query parameter 'from_ts' must be a non-negative integer")
+		}
+	}
+
+	untilTS := unspecifiedTS
+	if len(qs["until_ts"]) > 0 {
+		untilTS, err = strconv.ParseInt(qs.Get("until_ts"), 10, 64)
+		if err != nil || untilTS < 0 {
+			return api.BadRequest("Query parameter 'until_ts' must be a non-negative integer")
+		} else if untilTS <= fromTS {
+			return api.BadRequest("Query parameter 'until_ts' must be greater than 'from_ts'")
+		}
+	}
+
+	searchTerm := qs.Get("search_term")
+	if searchTerm == "" && len(qs["search_term"]) > 0 {
+		return api.BadRequest("Query parameter 'search_term' cannot be an empty string")
+	}
+
+	isAscendingOrder := true
+	direction := qs.Get("dir")
+	if direction == "f" || len(qs["dir"]) == 0 {
+		// Use default order
+	} else if direction == "b" {
+		isAscendingOrder = false
+	} else {
+		return api.BadRequest("Query parameter 'dir' must be one of ['f', 'b']")
+	}
+
+	rctx = rctx.LogWithFields(logrus.Fields{
+		"serverName":       serverName,
+		"order_by":         orderBy,
+		"from":             start,
+		"limit":            limit,
+		"from_ts":          fromTS,
+		"until_ts":         untilTS,
+		"search_term":      searchTerm,
+		"isAscendingOrder": isAscendingOrder,
+	})
+
+	db := storage.GetDatabase().GetMediaStore(rctx)
+
+	stats, totalCount, err := db.GetUsersUsageStatsForServer(
+		serverName,
+		orderBy,
+		start,
+		limit,
+		fromTS,
+		untilTS,
+		searchTerm,
+		isAscendingOrder)
+
+	if err != nil {
+		rctx.Log.Error(err)
+		sentry.CaptureException(err)
+		return api.InternalServerError("Failed to get users' usage stats on specified server")
+	}
+
+	var users []map[string]interface{}
+	if len(stats) == 0 {
+		users = []map[string]interface{}{}
+	} else {
+		for _, record := range stats {
+			users = append(users, map[string]interface{}{
+				"media_count":  record.MediaCount,
+				"media_length": record.MediaLength,
+				"user_id":      record.UserId,
+			})
+		}
+	}
+
+	var result = map[string]interface{}{
+		"users": users,
+		"total": totalCount,
+	}
+	if (start + limit) < totalCount {
+		result["next_token"] = start + int64(len(stats))
+	}
+
+	return &api.DoNotCacheResponse{Payload: result}
 }
