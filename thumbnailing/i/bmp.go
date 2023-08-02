f9ff53d7bf188b30ba497cd059ca123a22d67016
Travis Ralston <travpc@gmail.com>
2023-08-01 18:07:07 -0600

    Add BMP thumbnail support

---
+++ thumbnailing/i/bmp.go
@@ -0,0 +1,47 @@
+package i
+
+import (
+	"errors"
+	"io"
+
+	"github.com/turt2live/matrix-media-repo/common/rcontext"
+	"github.com/turt2live/matrix-media-repo/thumbnailing/m"
+	"github.com/turt2live/matrix-media-repo/util"
+	"golang.org/x/image/bmp"
+)
+
+type bmpGenerator struct {
+}
+
+func (d bmpGenerator) supportedContentTypes() []string {
+	return []string{"image/bmp", "image/x-bmp"}
+}
+
+func (d bmpGenerator) supportsAnimation() bool {
+	return false
+}
+
+func (d bmpGenerator) matches(img io.Reader, contentType string) bool {
+	return util.ArrayContains(d.supportedContentTypes(), contentType)
+}
+
+func (d bmpGenerator) GetOriginDimensions(b io.Reader, contentType string, ctx rcontext.RequestContext) (bool, int, int, error) {
+	i, err := bmp.DecodeConfig(b)
+	if err != nil {
+		return false, 0, 0, err
+	}
+	return true, i.Width, i.Height, nil
+}
+
+func (d bmpGenerator) GenerateThumbnail(b io.Reader, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error) {
+	src, err := bmp.Decode(b)
+	if err != nil {
+		return nil, errors.New("bmp: error decoding thumbnail: " + err.Error())
+	}
+
+	return pngGenerator{}.GenerateThumbnailOf(src, width, height, method, ctx)
+}
+
+func init() {
+	generators = append(generators, bmpGenerator{})
+}
