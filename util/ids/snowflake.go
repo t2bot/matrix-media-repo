cda49dbfccb9fa5859056d07f104466e3d786d71
Travis Ralston <travpc@gmail.com>
2023-08-07 15:14:40 -0600

    Remove useless 1023 check in snowflake

--- util/ids/snowflake.go
+++ util/ids/snowflake.go
@@ -1,20 +1,15 @@
 package ids
 
 import (
-	"errors"
 	"os"
 	"strconv"
 
 	"github.com/bwmarrin/snowflake"
-	"github.com/turt2live/matrix-media-repo/common/config"
 )
 
 func GetMachineId() int64 {
 	if val, ok := os.LookupEnv("MACHINE_ID"); ok {
 		if i, err := strconv.ParseInt(val, 10, 64); err == nil {
-			if i == 1023 && !config.Runtime.IsImportProcess {
-				panic(errors.New("machine ID 1023 is reserved for use by import process"))
-			}
 			return i
 		}
 	}
