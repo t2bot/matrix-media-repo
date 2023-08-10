950cd561a5ffbf3decb7ff86ade2527840f5b1dd
Travis Ralston <travpc@gmail.com>
2023-08-10 01:31:15 -0600

    Finish the upload test

--- test/upload_suite_test.go
+++ test/upload_suite_test.go
@@ -1,7 +1,9 @@
 package test
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -35,13 +37,28 @@ func (s *UploadTestSuite) TearDownSuite() {
 func (s *UploadTestSuite) TestUpload() {
 	t := s.T()
 
-	client := s.deps.Homeservers[0].UnprivilegedUsers[0].WithCsUrl(s.deps.Machines[0].HttpUrl)
+	client1 := s.deps.Homeservers[0].UnprivilegedUsers[0].WithCsUrl(s.deps.Machines[0].HttpUrl)
+	client2 := &test_internals.MatrixClient{
+		ClientServerUrl: s.deps.Machines[1].HttpUrl,       // deliberately the second machine
+		ServerName:      s.deps.Homeservers[1].ServerName, // deliberately the second machine
+		AccessToken:     "",                               // no auth for downloads
+		UserId:          "",                               // no auth for downloads
+	}
 
 	contentType, img, err := test_internals.MakeTestImage(512, 512)
-	res, err := client.Upload("image"+util.ExtensionForContentType(contentType), contentType, img)
+	res, err := client1.Upload("image"+util.ExtensionForContentType(contentType), contentType, img)
 	assert.NoError(t, err)
-	log.Println(res.MxcUri) // TODO: Test download (from other instance) matches what we uploaded
 	assert.NotEmpty(t, res.MxcUri)
+
+	origin, mediaId, err := util.SplitMxc(res.MxcUri)
+	assert.NoError(t, err)
+	assert.Equal(t, origin, client1.ServerName)
+	assert.NotEmpty(t, mediaId)
+
+	raw, err := client2.DoRaw("GET", fmt.Sprintf("/_matrix/media/v3/download/%s/%s", origin, mediaId), nil, "", nil)
+	assert.NoError(t, err)
+	assert.Equal(t, raw.StatusCode, http.StatusOK)
+	test_internals.AssertIsTestImage(t, raw.Body)
 }
 
 func TestUploadTestSuite(t *testing.T) {
