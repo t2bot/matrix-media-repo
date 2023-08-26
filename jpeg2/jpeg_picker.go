8e68c194ec6958b6dd9c5c1c48a93519670aec37
Travis Ralston <travpc@gmail.com>
2023-08-26 14:49:13 -0600

    Revert "Don't even reference image/jpeg"
    This reverts commit 1ae78235da52c984705c99a53ae0f2bdc90f6214.

--- jpeg2/jpeg_picker.go
+++ jpeg2/jpeg_picker.go
@@ -2,17 +2,35 @@ package jpeg2
 
 import (
 	"image"
+	gojpeg "image/jpeg"
 	"io"
 
+	"github.com/turt2live/matrix-media-repo/util/readers"
 	libjpeg "github.com/viam-labs/go-libjpeg/jpeg"
 )
 
 func Decode(r io.Reader) (image.Image, error) {
-	return libjpeg.Decode(r, &libjpeg.DecoderOptions{})
+	br := readers.NewBufferReadsReader(r)
+	c, err := libjpeg.Decode(r, &libjpeg.DecoderOptions{})
+	if err != nil {
+		r = br.GetRewoundReader()
+		return gojpeg.Decode(r)
+	} else {
+		br.Discard()
+	}
+	return c, nil
 }
 
 func DecodeConfig(r io.Reader) (image.Config, error) {
-	return libjpeg.DecodeConfig(r)
+	br := readers.NewBufferReadsReader(r)
+	c, err := libjpeg.DecodeConfig(r)
+	if err != nil {
+		r = br.GetRewoundReader()
+		return gojpeg.DecodeConfig(r)
+	} else {
+		br.Discard()
+	}
+	return c, nil
 }
 
 func init() {
