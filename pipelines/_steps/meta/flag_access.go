fea0fc0a2ac067d66210f9c247e993035add7e33
Travis Ralston <travpc@gmail.com>
2023-08-25 20:47:39 -0600

    Fix units

--- pipelines/_steps/meta/flag_access.go
+++ pipelines/_steps/meta/flag_access.go
@@ -10,7 +10,7 @@ import (
 
 func FlagAccess(ctx rcontext.RequestContext, sha256hash string, uploadTime int64) {
 	if uploadTime > 0 {
-		metrics.MediaAgeAccessed.Observe(float64(util.NowMillis() - uploadTime))
+		metrics.MediaAgeAccessed.Observe(float64(util.NowMillis()-uploadTime) / 1000.0)
 	}
 	if err := database.GetInstance().LastAccess.Prepare(ctx).Upsert(sha256hash, util.NowMillis()); err != nil {
 		ctx.Log.Warnf("Non-fatal error while updating last access for '%s': %s", sha256hash, err.Error())
