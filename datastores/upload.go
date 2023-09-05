081a53aeb259f7fdb32e70f6c8f92d0ef5b6969b
Travis Ralston <travpc@gmail.com>
2023-09-05 12:47:27 -0600

    Don't rely on StatObject/FileExists checks
    Fixes https://github.com/turt2live/matrix-media-repo/issues/446
    
    We instead just generate something unique enough.

--- datastores/upload.go
+++ datastores/upload.go
@@ -6,7 +6,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"net/http"
 	"os"
 	"path"
 
@@ -22,8 +21,14 @@ func Upload(ctx rcontext.RequestContext, ds config.DatastoreConfig, data io.Read
 	defer data.Close()
 	hasher := sha256.New()
 	tee := io.TeeReader(data, hasher)
-	var objectName string
-	var err error
+
+	objectName, err := ids.NewUniqueId()
+	if err != nil {
+		return "", err
+	}
+
+	// Suffix the ID so file paths are correctly bucketed
+	objectName = fmt.Sprintf("%sidv2fmt", objectName)
 
 	var uploadedBytes int64
 	if ds.Type == "s3" {
@@ -31,31 +36,6 @@ func Upload(ctx rcontext.RequestContext, ds config.DatastoreConfig, data io.Read
 		s3c, err = getS3(ds)
 		if err != nil {
 			return "", err
-		}
-
-		// Ensure unique ID
-		exists := true
-		attempts := 0
-		for exists {
-			objectName, err = ids.NewUniqueId()
-			if err != nil {
-				return "", err
-			}
-
-			attempts++
-			if attempts > 10 {
-				return "", errors.New("failed to generate suitable object name for S3 store")
-			}
-			metrics.S3Operations.With(prometheus.Labels{"operation": "StatObject"}).Inc()
-			_, err = s3c.client.StatObject(ctx.Context, s3c.bucket, objectName, minio.StatObjectOptions{})
-			if err != nil {
-				var merr minio.ErrorResponse
-				if errors.As(err, &merr) {
-					if merr.Code == "NoSuchKey" || merr.StatusCode == http.StatusNotFound {
-						exists = false
-					}
-				}
-			}
 		}
 
 		metrics.S3Operations.With(prometheus.Labels{"operation": "PutObject"}).Inc()
@@ -71,34 +51,12 @@ func Upload(ctx rcontext.RequestContext, ds config.DatastoreConfig, data io.Read
 		var targetDir string
 		var targetFile string
 
-		// Ensure unique ID
-		exists := true
-		attempts := 0
-		for exists {
-			objectName, err = ids.NewUniqueId()
-			if err != nil {
-				return "", err
-			}
-
-			attempts++
-			if attempts > 10 {
-				return "", errors.New("failed to generate suitable file name for persistence")
-			}
-
-			firstContainer = objectName[0:2]
-			secondContainer = objectName[2:4]
-			fileName = objectName[4:]
-			objectName = path.Join(firstContainer, secondContainer, fileName)
-			targetDir = path.Join(basePath, firstContainer, secondContainer)
-			targetFile = path.Join(targetDir, fileName)
-
-			_, err = os.Stat(targetFile)
-			if err != nil && !os.IsNotExist(err) {
-				return "", err
-			} else if err != nil && os.IsNotExist(err) {
-				exists = false
-			}
-		}
+		firstContainer = objectName[0:2]
+		secondContainer = objectName[2:4]
+		fileName = objectName[4:]
+		objectName = path.Join(firstContainer, secondContainer, fileName)
+		targetDir = path.Join(basePath, firstContainer, secondContainer)
+		targetFile = path.Join(targetDir, fileName)
 
 		// Persist file
 		var file *os.File
