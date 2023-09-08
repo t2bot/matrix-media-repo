12dc599477d68ecde13932b5b56443cb01df154f
Travis Ralston <travpc@gmail.com>
2023-09-08 13:06:53 -0600

    Use runtime/debug to get the git version
    Slightly smaller ldflags, or something

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ set -ex
 
 GOBIN=$PWD/bin go install -v ./cmd/compile_assets
 $PWD/bin/compile_assets
-GOBIN=$PWD/bin go install -ldflags "-X github.com/turt2live/matrix-media-repo/common/version.GitCommit=$(git rev-list -1 HEAD) -X github.com/turt2live/matrix-media-repo/common/version.Version=$(git describe --tags)" -v ./cmd/...
-GOBIN=$PWD/bin go install -pgo=pgo_media_repo.pprof -ldflags "-X github.com/turt2live/matrix-media-repo/common/version.GitCommit=$(git rev-list -1 HEAD) -X github.com/turt2live/matrix-media-repo/common/version.Version=$(git describe --tags)" -v ./cmd/media_repo
+GOBIN=$PWD/bin go install -ldflags "-X github.com/turt2live/matrix-media-repo/common/version.Version=$(git describe --tags)" -v ./cmd/...
+GOBIN=$PWD/bin go install -pgo=pgo_media_repo.pprof -ldflags "-X github.com/turt2live/matrix-media-repo/common/version.Version=$(git describe --tags)" -v ./cmd/media_repo
--- common/version/version.go
+++ common/version/version.go
@@ -2,6 +2,7 @@ package version
 
 import (
 	"fmt"
+	"runtime/debug"
 
 	"github.com/sirupsen/logrus"
 )
@@ -10,9 +11,20 @@ var GitCommit string
 var Version string
 
 func SetDefaults() {
+	build, infoOk := debug.ReadBuildInfo()
+
 	if GitCommit == "" {
 		GitCommit = ".dev"
+		if infoOk {
+			for _, setting := range build.Settings {
+				if setting.Key == "vcs.revision" {
+					GitCommit = setting.Value
+					break
+				}
+			}
+		}
 	}
+
 	if Version == "" {
 		Version = "unknown"
 	}
