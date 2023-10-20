4b93c9cd47b26e5af7e751202589d9eae56badfc
Travis Ralston <travpc@gmail.com>
2023-10-20 16:58:08 -0600

    Pick a version of libheif specifically

--- .github/workflows/build-libheif.sh
+++ .github/workflows/build-libheif.sh
@@ -3,9 +3,10 @@ set -ex
 sudo apt-get install -y git cmake make pkg-config libx265-dev libde265-dev libjpeg-dev libtool
 git clone https://github.com/strukturag/libheif.git
 cd libheif
+git checkout v1.17.1
 mkdir build
 cd build
-cmake ..
+cmake --preset=release ..
 make
 sudo make install
 sudo ldconfig
