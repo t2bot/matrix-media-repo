d2bafc35a7f50a87b9a2f161be530bb9debf6d3f
Travis Ralston <travpc@gmail.com>
2023-11-19 21:22:29 -0700

    Reorganize `cmd` directory

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-GOBIN=$PWD/bin go install -v ./cmd/compile_assets
+GOBIN=$PWD/bin go install -v ./cmd/utilities/compile_assets
 $PWD/bin/compile_assets
 GOBIN=$PWD/bin go install -ldflags "-X github.com/turt2live/matrix-media-repo/common/version.Version=$(git describe --tags)" -v ./cmd/...
-GOBIN=$PWD/bin go install -pgo=pgo_media_repo.pprof -ldflags "-X github.com/turt2live/matrix-media-repo/common/version.Version=$(git describe --tags)" -v ./cmd/media_repo
+GOBIN=$PWD/bin go install -pgo=pgo_media_repo.pprof -ldflags "-X github.com/turt2live/matrix-media-repo/common/version.Version=$(git describe --tags)" -v ./cmd/workers/media_repo
