adb27ee819039fc2673485f796c540e994e3e062
Travis Ralston <travis+github@t2bot.io>
2023-11-22 20:36:08 -0700

    Add Dendrite online importer (#501)
    * Factor out common functions to utilities
    
    * Add Dendrite importer

--- cmd/homeserver_live_importers/import_dendrite/main.go
+++ cmd/homeserver_live_importers/import_dendrite/main.go
@@ -3,26 +3,26 @@ package main
 import (
 	"github.com/turt2live/matrix-media-repo/cmd/homeserver_live_importers/_common"
 	"github.com/turt2live/matrix-media-repo/common/rcontext"
-	"github.com/turt2live/matrix-media-repo/homeserver_interop/synapse"
+	"github.com/turt2live/matrix-media-repo/homeserver_interop/dendrite"
 )
 
 func main() {
-	cfg := _common.InitImportPsqlMatrixDownload("Synapse")
+	cfg := _common.InitImportPsqlMatrixDownload("Dendrite")
 	ctx := rcontext.InitialNoConfig()
 
 	ctx.Log.Debug("Connecting to homeserver database...")
-	hsDb, err := synapse.OpenDatabase(cfg.ConnectionString)
+	hsDb, err := dendrite.OpenDatabase(cfg.ConnectionString, cfg.ServerName)
 	if err != nil {
 		panic(err)
 	}
 
-	_common.PsqlMatrixDownloadCopy[synapse.LocalMedia](ctx, cfg, hsDb, func(record *synapse.LocalMedia) (*_common.MediaMetadata, error) {
+	_common.PsqlMatrixDownloadCopy[dendrite.LocalMedia](ctx, cfg, hsDb, func(record *dendrite.LocalMedia) (*_common.MediaMetadata, error) {
 		return &_common.MediaMetadata{
 			MediaId:        record.MediaId,
 			ContentType:    record.ContentType,
 			FileName:       record.UploadName,
 			UploaderUserId: record.UserId,
-			SizeBytes:      record.SizeBytes,
+			SizeBytes:      record.FileSizeBytes,
 		}, nil
 	})
 }
