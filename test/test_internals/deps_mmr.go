c13208e551554636328fac1081e5fb8d8d73c096
Travis Ralston <travpc@gmail.com>
2023-11-30 20:08:19 -0700

    Use built-in `KeepImage: true` from go-testcontainers

--- test/test_internals/deps_mmr.go
+++ test/test_internals/deps_mmr.go
@@ -2,7 +2,6 @@ package test_internals
 
 import (
 	"context"
-	"errors"
 	"fmt"
 	"io"
 	"log"
@@ -10,10 +9,8 @@ import (
 	"path"
 	"strconv"
 	"strings"
-	"sync/atomic"
 	"text/template"
 
-	"github.com/docker/docker/api/types"
 	"github.com/docker/go-connections/nat"
 	"github.com/testcontainers/testcontainers-go"
 	"github.com/testcontainers/testcontainers-go/wait"
@@ -38,53 +35,6 @@ type mmrContainer struct {
 
 	HttpUrl   string
 	MachineId int
-}
-
-var mmrCachedImage string
-var mmrCachedContext *os.File
-var mmrCachedSuites atomic.Int32
-
-func MarkUsingCachedMmrBuild() {
-	mmrCachedSuites.Add(1)
-	log.Println("+1 cached build usage")
-}
-
-func reuseMmrBuild(ctx context.Context) (string, error) {
-	if mmrCachedImage != "" {
-		return mmrCachedImage, nil
-	}
-	log.Println("[Test Deps] Building MMR image...")
-	cr, err := createDockerContext()
-	if err != nil {
-		return "", err
-	}
-	mmrCachedContext = cr
-	buildReq := testcontainers.GenericContainerRequest{
-		ContainerRequest: testcontainers.ContainerRequest{
-			FromDockerfile: testcontainers.FromDockerfile{
-				Dockerfile:     "Dockerfile",
-				Context:        ".",
-				ContextArchive: cr,
-				PrintBuildLog:  true,
-			},
-		},
-		Started: false,
-	}
-	provider, err := buildReq.ProviderType.GetProvider(testcontainers.WithLogger(testcontainers.Logger))
-	if err != nil {
-		return "", err
-	}
-	if dockerProvider, ok := provider.(*testcontainers.DockerProvider); !ok {
-		return "", errors.New("expected a docker provider")
-	} else {
-		tag, err := dockerProvider.BuildImage(ctx, &buildReq)
-		if err != nil {
-			return "", err
-		}
-		mmrCachedImage = tag
-	}
-	log.Println("[Test Deps] Cached build as ", mmrCachedImage)
-	return mmrCachedImage, nil
 }
 
 func writeMmrConfig(tmplArgs mmrTmplArgs) (string, error) {
@@ -126,20 +76,24 @@ func makeMmrInstances(ctx context.Context, count int, depNet *NetworkDep, tmplAr
 		return nil, err
 	}
 
-	// Cache the MMR image
-	mmrImage, err := reuseMmrBuild(ctx)
-	if err != nil {
-		return nil, err
-	}
-
 	// Start the containers (using the same DB and config)
 	mmrs := make([]*mmrContainer, 0)
 	for i := 0; i < count; i++ {
 		// Create the docker container (from dockerfile)
+		cr, err := createDockerContext()
+		if err != nil {
+			return nil, err
+		}
 		p, _ := nat.NewPort("tcp", "8000")
 		container, err := testcontainers.GenericContainer(ctx, testcontainers.GenericContainerRequest{
 			ContainerRequest: testcontainers.ContainerRequest{
-				Image:        mmrImage,
+				FromDockerfile: testcontainers.FromDockerfile{
+					Dockerfile:     "Dockerfile",
+					Context:        ".",
+					ContextArchive: cr,
+					PrintBuildLog:  true,
+					KeepImage:      true,
+				},
 				ExposedPorts: []string{"8000/tcp"},
 				Mounts: []testcontainers.ContainerMount{
 					testcontainers.BindMount(intTmpName, "/data/media-repo.yaml"),
@@ -192,44 +146,4 @@ func (c *mmrContainer) Teardown() {
 
 func (c *mmrContainer) Logs() (io.ReadCloser, error) {
 	return c.container.Logs(c.ctx)
-}
-
-func TeardownMmrCaches() {
-	mmrCachedSuites.Add(-1)
-	if mmrCachedSuites.Load() > 0 {
-		log.Println("Not cleaning up MMR cached images: image still in use")
-		return
-	}
-
-	if mmrCachedContext != nil {
-		_ = mmrCachedContext.Close() // ignore errors because testcontainers might have already closed it
-		if err := os.Remove(mmrCachedContext.Name()); err != nil && !os.IsNotExist(err) {
-			log.Fatalf("Error cleaning up MMR cached context file '%s': %s", mmrCachedContext.Name(), err.Error())
-		}
-	}
-	if mmrCachedImage != "" {
-		if p, err := (testcontainers.GenericContainerRequest{}.ProviderType.GetProvider()); err != nil {
-			log.Fatalf("Error cleaning up MMR cached build image '%s': %s", mmrCachedImage, err.Error())
-		} else if dockerProvider, ok := p.(*testcontainers.DockerProvider); !ok {
-			log.Fatalf("Error cleaning up MMR cached build image '%s': unable to cast provider to DockerProvider", mmrCachedImage)
-		} else {
-			rmImage := func(imageName string) {
-				if _, err = dockerProvider.Client().ImageRemove(context.Background(), imageName, types.ImageRemoveOptions{
-					PruneChildren: true,
-				}); err != nil {
-					log.Fatalf("Error removing MMR cached build image '%s': %s", imageName, err.Error())
-				}
-			}
-			if images, err := dockerProvider.Client().ImageList(context.Background(), types.ImageListOptions{All: true}); err != nil {
-				log.Fatalf("Error listing Docker images to clean up MMR image '%s': %s", mmrCachedImage, err.Error())
-			} else {
-				rmImage(mmrCachedImage)
-				for _, i := range images {
-					if i.Labels != nil && i.Labels["io.t2bot.mmr.cleanup"] == "true" {
-						rmImage(i.ID)
-					}
-				}
-			}
-		}
-	}
 }
