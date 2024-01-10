3915d58b41a7a8c0d8d8ee2ad00a20beae0dabfe
Sophie Tauchert <github@999eagle.moe>
2024-01-10 17:29:58 +0100

    Make sure the buffer temp path is writable (#490)

--- datastores/buffer.go
+++ datastores/buffer.go
@@ -32,7 +32,7 @@ func BufferTemp(datastore config.DatastoreConfig, contents io.ReadCloser) (strin
 		logrus.Warnf("Datastore %s does not have a valid temporary path configured. This will lead to increased memory usage.", datastore.Id)
 		target = &bytes.Buffer{}
 	} else {
-		err = os.Mkdir(fpath, os.ModeDir)
+		err = os.Mkdir(fpath, os.ModeDir | 0700)
 		if err != nil && !os.IsExist(err) {
 			return "", 0, nil, errors.New("error creating temp path: " + err.Error())
 		}
