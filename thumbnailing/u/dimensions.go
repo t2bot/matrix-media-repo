e890b41a57e6771b73e9d583b434e558bd05db6d
Travis Ralston <travis@t2bot.io>
2024-02-13 14:55:50 -0700

    Remove unused animated return

--- thumbnailing/u/dimensions.go
+++ thumbnailing/u/dimensions.go
@@ -1,6 +1,6 @@
 package u
 
-func AdjustProperties(srcWidth int, srcHeight int, desiredWidth int, desiredHeight int, wantAnimated bool, method string) (bool, int, int, bool, string) {
+func AdjustProperties(srcWidth int, srcHeight int, desiredWidth int, desiredHeight int, wantAnimated bool, method string) (bool, int, int, string) {
 	aspectRatio := float32(srcHeight) / float32(srcWidth)
 	targetAspectRatio := float32(desiredHeight) / float32(desiredWidth)
 	if aspectRatio == targetAspectRatio {
@@ -10,10 +10,10 @@ func AdjustProperties(srcWidth int, srcHeight int, desiredWidth int, desiredHeig
 
 	if srcWidth <= desiredWidth && srcHeight <= desiredHeight {
 		if wantAnimated {
-			return true, srcWidth, srcHeight, true, method
+			return true, srcWidth, srcHeight, method
 		} else {
-			return false, desiredWidth, desiredHeight, false, method
+			return false, desiredWidth, desiredHeight, method
 		}
 	}
-	return true, desiredWidth, desiredHeight, wantAnimated, method
+	return true, desiredWidth, desiredHeight, method
 }
