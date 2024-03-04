t2bot/matrix-media-repo

commit 4378a4ec07aec6e5fc1ed98d93bce9318017aff3

Avoid use of buffers throughout thumbnailing
For https://github.com/turt2live/matrix-media-repo/issues/413

thumbnailing/i/gif.go
@@ -1,7 +1,6 @@
 package i
 
 import (
-	"bytes"
 	"errors"
 	"image"
 	"image/draw"
@@ -25,16 +24,16 @@ func (d gifGenerator) supportsAnimation() bool {
 	return true
 }
 
-func (d gifGenerator) matches(img []byte, contentType string) bool {
+func (d gifGenerator) matches(img io.Reader, contentType string) bool {
 	return contentType == "image/gif"
 }
 
-func (d gifGenerator) GetOriginDimensions(b []byte, contentType string, ctx rcontext.RequestContext) (bool, int, int, error) {
+func (d gifGenerator) GetOriginDimensions(b io.Reader, contentType string, ctx rcontext.RequestContext) (bool, int, int, error) {
 	return pngGenerator{}.GetOriginDimensions(b, contentType, ctx)
 }
 
-func (d gifGenerator) GenerateThumbnail(b []byte, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error) {
-	g, err := gif.DecodeAll(bytes.NewBuffer(b))
+func (d gifGenerator) GenerateThumbnail(b io.Reader, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error) {
+	g, err := gif.DecodeAll(b)
 	if err != nil {
 		return nil, errors.New("gif: error decoding image: " + err.Error())
 	}
@@ -77,15 +76,19 @@ func (d gifGenerator) GenerateThumbnail(b []byte, contentType string, width int,
 			}
 
 			// The thumbnailer decided that it shouldn't thumbnail, so encode it ourselves
-			buf := &bytes.Buffer{}
-			err = imaging.Encode(buf, targetImg, imaging.PNG)
-			if err != nil {
-				return nil, errors.New("gif: error encoding still frame thumbnail: " + err.Error())
-			}
+			pr, pw := io.Pipe()
+			go func(pw *io.PipeWriter, p *image.Paletted) {
+				err = imaging.Encode(pw, p, imaging.PNG)
+				if err != nil {
+					_ = pw.CloseWithError(errors.New("gif: error encoding still frame thumbnail: " + err.Error()))
+				} else {
+					_ = pw.Close()
+				}
+			}(pw, targetImg)
 			return &m.Thumbnail{
 				Animated:    false,
 				ContentType: "image/png",
-				Reader:      io.NopCloser(buf),
+				Reader:      pr,
 			}, nil
 		}
 
@@ -107,16 +110,20 @@ func (d gifGenerator) GenerateThumbnail(b []byte, contentType string, width int,
 	g.Config.Width = g.Image[0].Bounds().Max.X
 	g.Config.Height = g.Image[0].Bounds().Max.Y
 
-	buf := &bytes.Buffer{}
-	err = gif.EncodeAll(buf, g)
-	if err != nil {
-		return nil, errors.New("gif: error encoding final thumbnail: " + err.Error())
-	}
+	pr, pw := io.Pipe()
+	go func(pw *io.PipeWriter, g *gif.GIF) {
+		err = gif.EncodeAll(pw, g)
+		if err != nil {
+			_ = pw.CloseWithError(errors.New("gif: error encoding final thumbnail: " + err.Error()))
+		} else {
+			_ = pw.Close()
+		}
+	}(pw, g)
 
 	return &m.Thumbnail{
 		ContentType: "image/gif",
 		Animated:    true,
-		Reader:      io.NopCloser(buf),
+		Reader:      pr,
 	}, nil
 }
 

api/unstable/info.go
@@ -137,10 +137,10 @@ func MediaInfo(r *http.Request, rctx rcontext.RequestContext, user _apimeta.User
 	}
 
 	if strings.HasPrefix(response.ContentType, "audio/") {
-		generator, err := thumbnailing.GetGenerator(util_byte_seeker.NewByteSeeker(b), response.ContentType, false)
+		generator, reconstructed, err := thumbnailing.GetGenerator(util_byte_seeker.NewByteSeeker(b), response.ContentType, false)
 		if err == nil {
 			if audiogenerator, ok := generator.(i.AudioGenerator); ok {
-				audioInfo, err := audiogenerator.GetAudioData(b, 768, rctx)
+				audioInfo, err := audiogenerator.GetAudioData(reconstructed, 768, rctx)
 				if err == nil {
 					response.KeySamples = audioInfo.KeySamples
 					response.NumChannels = audioInfo.Channels

datastores/buffer.go
@@ -10,6 +10,7 @@ import (
 
 	"github.com/sirupsen/logrus"
 	"github.com/turt2live/matrix-media-repo/common/config"
+	"github.com/turt2live/matrix-media-repo/util/readers"
 )
 
 func BufferTemp(datastore config.DatastoreConfig, contents io.ReadCloser) (string, int64, io.ReadCloser, error) {
@@ -65,46 +66,10 @@ func BufferTemp(datastore config.DatastoreConfig, contents io.ReadCloser) (strin
 		if err != nil {
 			return "", 0, nil, err
 		}
-		return hash(), sizeBytes, &tempFileCloser{
-			fname:    f.Name(),
-			fpath:    fpath,
-			upstream: f,
-			closed:   false,
-		}, nil
+		return hash(), sizeBytes, readers.NewTempFileCloser(fpath, f.Name(), f), nil
 	} else if b, ok := target.(*bytes.Buffer); ok {
 		return hash(), sizeBytes, io.NopCloser(b), nil
 	} else {
 		return "", 0, nil, errors.New("developer error - did not account for possible stream writer type")
 	}
-}
-
-type tempFileCloser struct {
-	io.ReadCloser
-	fname    string
-	fpath    string
-	upstream io.ReadCloser
-	closed   bool
-}
-
-func (c *tempFileCloser) Close() error {
-	if c.closed {
-		return nil
-	}
-	var upstreamErr error
-	if upstreamErr = c.upstream.Close(); upstreamErr != nil {
-		// don't return the error yet because we want to try to delete the temp file
-	}
-	var err error
-	if err = os.Remove(c.fname); err != nil {
-		return err
-	}
-	if err = os.Remove(c.fpath); err != nil {
-		return err
-	}
-	c.closed = true
-	return upstreamErr
-}
-
-func (c *tempFileCloser) Read(p []byte) (n int, err error) {
-	return c.upstream.Read(p)
 }

thumbnailing/i/01-factories.go
@@ -1,38 +1,43 @@
 package i
 
 import (
+	"io"
+
 	"github.com/turt2live/matrix-media-repo/common/rcontext"
 	"github.com/turt2live/matrix-media-repo/thumbnailing/m"
+	"github.com/turt2live/matrix-media-repo/util/readers"
 )
 
 type Generator interface {
 	supportedContentTypes() []string
 	supportsAnimation() bool
-	matches(img []byte, contentType string) bool
-	GenerateThumbnail(b []byte, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error)
-	GetOriginDimensions(b []byte, contentType string, ctx rcontext.RequestContext) (bool, int, int, error)
+	matches(img io.Reader, contentType string) bool
+	GenerateThumbnail(img io.Reader, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error)
+	GetOriginDimensions(b io.Reader, contentType string, ctx rcontext.RequestContext) (bool, int, int, error)
 }
 
 type AudioGenerator interface {
-	GetAudioData(b []byte, nKeys int, ctx rcontext.RequestContext) (*m.AudioInfo, error)
+	Generator
+	GetAudioData(b io.Reader, nKeys int, ctx rcontext.RequestContext) (*m.AudioInfo, error)
 }
 
 var generators = make([]Generator, 0)
 
-func GetGenerator(img []byte, contentType string, needsAnimation bool) Generator {
+func GetGenerator(img io.Reader, contentType string, needsAnimation bool) (Generator, *readers.PrefixedReader) {
+	br := readers.NewBufferReadsReader(img)
 	for _, g := range generators {
 		if needsAnimation && !g.supportsAnimation() {
 			continue
 		}
-		if g.matches(img, contentType) {
-			return g
+		if g.matches(br, contentType) {
+			return g, br.GetRewoundReader()
 		}
 	}
 	if needsAnimation {
 		// try again, this time without animation
-		return GetGenerator(img, contentType, false)
+		return GetGenerator(br.GetRewoundReader(), contentType, false)
 	}
-	return nil
+	return nil, br.GetRewoundReader()
 }
 
 func GetSupportedContentTypes() []string {

thumbnailing/i/apng.go
@@ -1,16 +1,15 @@
 package i
 
 import (
-	"bytes"
 	"errors"
 	"image"
 	"image/draw"
 	"io"
 
+	"github.com/getsentry/sentry-go"
 	"github.com/kettek/apng"
 	"github.com/turt2live/matrix-media-repo/common/rcontext"
 	"github.com/turt2live/matrix-media-repo/thumbnailing/m"
-	"github.com/turt2live/matrix-media-repo/util"
 )
 
 type apngGenerator struct {
@@ -24,24 +23,24 @@ func (d apngGenerator) supportsAnimation() bool {
 	return true
 }
 
-func (d apngGenerator) matches(img []byte, contentType string) bool {
-	return (contentType == "image/png" && util.IsAnimatedPNG(img)) || contentType == "image/apng"
+func (d apngGenerator) matches(img io.Reader, contentType string) bool {
+	return (contentType == "image/png" && isAnimatedPNG(img)) || contentType == "image/apng"
 }
 
-func (d apngGenerator) GetOriginDimensions(b []byte, contentType string, ctx rcontext.RequestContext) (bool, int, int, error) {
-	i, err := apng.DecodeConfig(bytes.NewBuffer(b))
+func (d apngGenerator) GetOriginDimensions(b io.Reader, contentType string, ctx rcontext.RequestContext) (bool, int, int, error) {
+	i, err := apng.DecodeConfig(b)
 	if err != nil {
 		return false, 0, 0, err
 	}
 	return true, i.Width, i.Height, nil
 }
 
-func (d apngGenerator) GenerateThumbnail(b []byte, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error) {
+func (d apngGenerator) GenerateThumbnail(b io.Reader, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error) {
 	if !animated {
 		return pngGenerator{}.GenerateThumbnail(b, "image/png", width, height, method, false, ctx)
 	}
 
-	p, err := apng.DecodeAll(bytes.NewBuffer(b))
+	p, err := apng.DecodeAll(b)
 	if err != nil {
 		return nil, errors.New("apng: error decoding image: " + err.Error())
 	}
@@ -90,19 +89,64 @@ func (d apngGenerator) GenerateThumbnail(b []byte, contentType string, width int
 		}
 	}
 
-	buf := &bytes.Buffer{}
-	err = apng.Encode(buf, p)
-	if err != nil {
-		return nil, errors.New("apng: error encoding final thumbnail: " + err.Error())
-	}
+	pr, pw := io.Pipe()
+	go func(pw *io.PipeWriter, p apng.APNG) {
+		err = apng.Encode(pw, p)
+		if err != nil {
+			_ = pw.CloseWithError(errors.New("apng: error encoding final thumbnail: " + err.Error()))
+		} else {
+			_ = pw.Close()
+		}
+	}(pw, p)
 
 	return &m.Thumbnail{
 		ContentType: "image/png",
 		Animated:    true,
-		Reader:      io.NopCloser(buf),
+		Reader:      pr,
 	}, nil
 }
 
 func init() {
 	generators = append(generators, apngGenerator{})
+}
+
+func isAnimatedPNG(r io.Reader) bool {
+	maxBytes := 4096 // if we don't have an acTL chunk after 4kb, give up
+	IDAT := []byte{0x49, 0x44, 0x41, 0x54}
+	acTL := []byte{0x61, 0x63, 0x54, 0x4C}
+
+	b := make([]byte, maxBytes)
+	c, err := r.Read(b)
+	if err != nil {
+		// we don't log the error, but we do want to report it if sentry is hooked up
+		sentry.CaptureException(err)
+		return false // assume read errors are a problem
+	}
+
+	idatIdx := 0
+	actlIdx := 0
+	for i, bt := range b {
+		if i > c {
+			break
+		}
+		if bt == IDAT[idatIdx] {
+			idatIdx++
+			actlIdx = 0
+		} else if bt == acTL[actlIdx] {
+			actlIdx++
+			idatIdx = 0
+		} else {
+			idatIdx = 0
+			actlIdx = 0
+		}
+
+		if idatIdx == len(IDAT) {
+			return false
+		}
+		if actlIdx == len(acTL) {
+			return true
+		}
+	}
+
+	return false
 }

thumbnailing/i/flac.go
@@ -2,13 +2,13 @@ package i
 
 import (
 	"errors"
+	"io"
 
 	"github.com/faiface/beep"
 	"github.com/faiface/beep/flac"
 	"github.com/turt2live/matrix-media-repo/common/rcontext"
 	"github.com/turt2live/matrix-media-repo/thumbnailing/m"
 	"github.com/turt2live/matrix-media-repo/thumbnailing/u"
-	"github.com/turt2live/matrix-media-repo/util/util_byte_seeker"
 )
 
 type flacGenerator struct {
@@ -22,38 +22,47 @@ func (d flacGenerator) supportsAnimation() bool {
 	return false
 }
 
-func (d flacGenerator) matches(img []byte, contentType string) bool {
+func (d flacGenerator) matches(img io.Reader, contentType string) bool {
 	return contentType == "audio/flac"
 }
 
-func (d flacGenerator) decode(b []byte) (beep.StreamSeekCloser, beep.Format, error) {
-	audio, format, err := flac.Decode(util_byte_seeker.NewByteSeeker(b))
+func (d flacGenerator) decode(b io.Reader) (beep.StreamSeekCloser, beep.Format, error) {
+	audio, format, err := flac.Decode(b)
 	if err != nil {
 		return audio, format, errors.New("flac: error decoding audio: " + err.Error())
 	}
 	return audio, format, nil
 }
 
-func (d flacGenerator) GetOriginDimensions(b []byte, contentType string, ctx rcontext.RequestContext) (bool, int, int, error) {
+func (d flacGenerator) GetOriginDimensions(b io.Reader, contentType string, ctx rcontext.RequestContext) (bool, int, int, error) {
 	return false, 0, 0, nil
 }
 
-func (d flacGenerator) GenerateThumbnail(b []byte, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error) {
-	audio, format, err := d.decode(b)
+func (d flacGenerator) GenerateThumbnail(r io.Reader, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error) {
+	tags, rc, err := u.GetID3Tags(r)
+	if err != nil {
+		return nil, errors.New("flac: error getting tags: " + err.Error())
+	}
+	//goland:noinspection GoUnhandledErrorResult
+	defer rc.Close()
+
+	audio, format, err := d.decode(rc)
 	if err != nil {
 		return nil, err
 	}
 
+	//goland:noinspection GoUnhandledErrorResult
 	defer audio.Close()
-	return mp3Generator{}.GenerateFromStream(audio, format, u.GetID3Tags(b), width, height)
+	return mp3Generator{}.GenerateFromStream(audio, format, tags, width, height)
 }
 
-func (d flacGenerator) GetAudioData(b []byte, nKeys int, ctx rcontext.RequestContext) (*m.AudioInfo, error) {
+func (d flacGenerator) GetAudioData(b io.Reader, nKeys int, ctx rcontext.RequestContext) (*m.AudioInfo, error) {
 	audio, format, err := d.decode(b)
 	if err != nil {
 		return nil, err
 	}
 
+	//goland:noinspection GoUnhandledErrorResult
 	defer audio.Close()
 	return mp3Generator{}.GetDataFromStream(audio, format, nKeys)
 }

thumbnailing/i/heif.go
@@ -1,8 +1,8 @@
 package i
 
 import (
-	"bytes"
 	"errors"
+	"io"
 
 	"github.com/adrium/goheif"
 	"github.com/turt2live/matrix-media-repo/common/rcontext"
@@ -20,23 +20,23 @@ func (d heifGenerator) supportsAnimation() bool {
 	return true
 }
 
-func (d heifGenerator) matches(img []byte, contentType string) bool {
+func (d heifGenerator) matches(img io.Reader, contentType string) bool {
 	return contentType == "image/heif"
 }
 
-func (d heifGenerator) GetOriginDimensions(b []byte, contentType string, ctx rcontext.RequestContext) (bool, int, int, error) {
-	i, err := goheif.DecodeConfig(bytes.NewBuffer(b))
+func (d heifGenerator) GetOriginDimensions(b io.Reader, contentType string, ctx rcontext.RequestContext) (bool, int, int, error) {
+	i, err := goheif.DecodeConfig(b)
 	if err != nil {
 		return false, 0, 0, err
 	}
 	return true, i.Width, i.Height, nil
 }
 
-func (d heifGenerator) GenerateThumbnail(b []byte, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error) {
+func (d heifGenerator) GenerateThumbnail(b io.Reader, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error) {
 	// Use more memory, but prevent crashes
 	goheif.SafeEncoding = true
 
-	src, err := goheif.Decode(bytes.NewBuffer(b))
+	src, err := goheif.Decode(b)
 	if err != nil {
 		return nil, errors.New("heif: error decoding thumbnail: " + err.Error())
 	}

thumbnailing/i/jpegxl.go
@@ -2,12 +2,10 @@ package i
 
 import (
 	"errors"
+	"io"
 	"os"
 	"os/exec"
 	"path"
-
-	"github.com/turt2live/matrix-media-repo/util/ids"
-	"github.com/turt2live/matrix-media-repo/util/stream_util"
 
 	"github.com/turt2live/matrix-media-repo/common/rcontext"
 	"github.com/turt2live/matrix-media-repo/thumbnailing/m"
@@ -24,44 +22,47 @@ func (d jpegxlGenerator) supportsAnimation() bool {
 	return false
 }
 
-func (d jpegxlGenerator) matches(img []byte, contentType string) bool {
+func (d jpegxlGenerator) matches(img io.Reader, contentType string) bool {
 	return contentType == "image/jxl"
 }
 
-func (d jpegxlGenerator) GetOriginDimensions(b []byte, contentType string, ctx rcontext.RequestContext) (bool, int, int, error) {
+func (d jpegxlGenerator) GetOriginDimensions(b io.Reader, contentType string, ctx rcontext.RequestContext) (bool, int, int, error) {
 	return false, 0, 0, nil
 }
 
-func (d jpegxlGenerator) GenerateThumbnail(b []byte, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error) {
-	key, err := ids.NewUniqueId()
+func (d jpegxlGenerator) GenerateThumbnail(b io.Reader, contentType string, width int, height int, method string, animated bool, ctx rcontext.RequestContext) (*m.Thumbnail, error) {
+	dir, err := os.MkdirTemp(os.TempDir(), "mmr-jpegxl")
 	if err != nil {
-		return nil, errors.New("jpegxl: error generating temp key: " + err.Error())
+		return nil, errors.New("jpegxl: error creating temporary directory: " + err.Error())
 	}
 
-	tempFile1 := path.Join(os.TempDir(), "media_repo."+key+".1.jpegxl")
-	tempFile2 := path.Join(os.TempDir(), "media_repo."+key+".2.png")
+	tempFile1 := path.Join(dir, "i.jpegxl")
+	tempFile2 := path.Join(dir, "o.png")
 
 	defer os.Remove(tempFile1)
 	defer os.Remove(tempFile2)
+	defer os.Remove(dir)
 
 	f, err := os.OpenFile(tempFile1, os.O_RDWR|os.O_CREATE, 0640)
 	if err != nil {
+		return nil, errors.New("jpegxl: error creating temp jpegxl file: " + err.Error())
+	}
+	if _, err = io.Copy(f, b); err != nil {
 		return nil, errors.New("jpegxl: error writing temp jpegxl file: " + err.Error())
 	}
-	_, _ = f.Write(b)
-	stream_util.DumpAndCloseStream(f)
 
 	err = exec.Command("convert", tempFile1, tempFile2).Run()
 	if err != nil {
 		return nil, errors.New("jpegxl: error converting jpegxl file: " + err.Error())
 	}
 
-	b, err = os.ReadFile(tempFile2)
+	f, err = os.OpenFile(tempFile2, os.O_RDONLY, 0640)
 	if err != nil {
 		return nil, errors.New("jpegxl: error reading temp png file: " + err.Error())
 	}
+	defer f.Close()
 
-	return pngGenerator{}.GenerateThumbnail(b, "image/png", width, height, method, false, ctx)
+	return pngGenerator{}.GenerateThumbnail(f, "image/png", width, height, method, false, ctx)
 }
 
 func init() {

commit 00f52fd92dc603e8f006176aa4d05221e39a2f77

fix memory leak in gif thumbnail encoding
When using an io.Pipe, it must be ensured that the pipe is drained. If
it is not, all references required for the goroutine will not be freed
by the garbage collector, as an active reference still exists (the
abandoned goroutine the pipe is written to.

To fix this, write to a non-blocking buffer. This may increase the RAM
usage in the short run, but can be fully garbage collected, even if not
read from. Since the size of the buffer is at most the size of the
thumbnail and it being freed quickly, this should post a significantly
smaller burden on the server.

Signed-off-by: Moritz Poldrack <git@moritz.sh>
Fixes: 4378a4e ("Avoid use of buffers throughout thumbnailing")
Fixes: #561

thumbnailing/i/gif.go
@@ -1,7 +1,9 @@
 package i
 
 import (
+	"bytes"
 	"errors"
+	"fmt"
 	"image"
 	"image/draw"
 	"image/gif"
@@ -76,19 +78,15 @@ func (d gifGenerator) GenerateThumbnail(b io.Reader, contentType string, width i
 			}
 
 			// The thumbnailer decided that it shouldn't thumbnail, so encode it ourselves
-			pr, pw := io.Pipe()
-			go func(pw *io.PipeWriter, p *image.Paletted) {
-				err = u.Encode(ctx, pw, p)
-				if err != nil {
-					_ = pw.CloseWithError(errors.New("gif: error encoding still frame thumbnail: " + err.Error()))
-				} else {
-					_ = pw.Close()
-				}
-			}(pw, targetImg)
+			buf := bytes.NewBuffer(make([]byte, 0, 128*1024))
+			err = u.Encode(ctx, buf, targetImg)
+			if err != nil {
+				return nil, fmt.Errorf("gif: error encoding static thumbnail: %w", err)
+			}
 			return &m.Thumbnail{
 				Animated:    false,
 				ContentType: "image/png",
-				Reader:      pr,
+				Reader:      io.NopCloser(buf),
 			}, nil
 		}
 
@@ -110,20 +108,16 @@ func (d gifGenerator) GenerateThumbnail(b io.Reader, contentType string, width i
 	g.Config.Width = g.Image[0].Bounds().Max.X
 	g.Config.Height = g.Image[0].Bounds().Max.Y
 
-	pr, pw := io.Pipe()
-	go func(pw *io.PipeWriter, g *gif.GIF) {
-		err = gif.EncodeAll(pw, g)
-		if err != nil {
-			_ = pw.CloseWithError(errors.New("gif: error encoding final thumbnail: " + err.Error()))
-		} else {
-			_ = pw.Close()
-		}
-	}(pw, g)
+	buf := bytes.NewBuffer(make([]byte, 0, 512*1024))
+	err = gif.EncodeAll(buf, g)
+	if err != nil {
+		return nil, fmt.Errorf("gif: error encoding animated thumbnail: %w", err)
+	}
 
 	return &m.Thumbnail{
 		ContentType: "image/gif",
 		Animated:    true,
-		Reader:      pr,
+		Reader:      io.NopCloser(buf),
 	}, nil
 }
 