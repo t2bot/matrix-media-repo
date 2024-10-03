95cdd15fe88c64e34ee91f0c8e491b1a7f639323
Andrew Ferrazzutti <andrewf@element.io>
2024-10-03 00:07:40 -0400

    Support MSC4189 (uploaded media access for guests)

---
+++ api/_routers/97-require-access-token-allow-guest.go
@@ -0,0 +1,55 @@
+package _routers
+
+import (
+	"errors"
+	"net/http"
+
+	"github.com/getsentry/sentry-go"
+	"github.com/sirupsen/logrus"
+	"github.com/t2bot/matrix-media-repo/api/_apimeta"
+	"github.com/t2bot/matrix-media-repo/api/_auth_cache"
+	"github.com/t2bot/matrix-media-repo/api/_responses"
+	"github.com/t2bot/matrix-media-repo/common"
+	"github.com/t2bot/matrix-media-repo/common/config"
+	"github.com/t2bot/matrix-media-repo/common/rcontext"
+	"github.com/t2bot/matrix-media-repo/matrix"
+	"github.com/t2bot/matrix-media-repo/util"
+)
+
+func RequireAccessTokenAllowGuest(generator GeneratorWithUserFn) GeneratorFn {
+	return func(r *http.Request, ctx rcontext.RequestContext) interface{} {
+		accessToken := util.GetAccessTokenFromRequest(r)
+		if accessToken == "" {
+			return &_responses.ErrorResponse{
+				Code:         common.ErrCodeMissingToken,
+				Message:      "no token provided (required)",
+				InternalCode: common.ErrCodeMissingToken,
+			}
+		}
+		if config.Get().SharedSecret.Enabled && accessToken == config.Get().SharedSecret.Token {
+			ctx = ctx.LogWithFields(logrus.Fields{"sharedSecretAuth": true})
+			return generator(r, ctx, _apimeta.UserInfo{
+				UserId:      "@sharedsecret",
+				AccessToken: accessToken,
+				IsShared:    true,
+			})
+		}
+		appserviceUserId := util.GetAppserviceUserIdFromRequest(r)
+		userId, err := _auth_cache.GetUserId(ctx, accessToken, appserviceUserId)
+		if err != nil || userId == "" {
+			if err != nil && !errors.Is(err, matrix.ErrInvalidToken) {
+				sentry.CaptureException(err)
+				ctx.Log.Error("Error verifying token: ", err)
+				return _responses.InternalServerError("unexpected error validating access token")
+			}
+			return _responses.AuthFailed()
+		}
+
+		ctx = ctx.LogWithFields(logrus.Fields{"authUserId": userId})
+		return generator(r, ctx, _apimeta.UserInfo{
+			UserId:      userId,
+			AccessToken: accessToken,
+			IsShared:    false,
+		})
+	}
+}
