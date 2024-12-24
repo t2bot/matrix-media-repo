02b492fe647c4e99ba7e179ca60fea73340de656
Travis Ralston <travis+github@t2bot.io>
2024-12-23 18:19:45 -0700

    Upgrade alpine & libheif version (#632)

--- .github/workflows/build-libheif.sh
+++ .github/workflows/build-libheif.sh
@@ -3,7 +3,7 @@ set -ex
 sudo apt-get install -y git cmake make pkg-config libx265-dev libde265-dev libjpeg-dev libtool
 git clone https://github.com/strukturag/libheif.git
 cd libheif
-git checkout v1.17.6
+git checkout v1.19.5
 mkdir build
 cd build
 cmake --preset=release ..
