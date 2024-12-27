2cf5a65f44b0f7312d39212d78e0d1ef8a379542
Travis Ralston <travis@t2bot.io>
2024-12-26 17:19:16 -0700

    Switch to an HMAC instead

--- api/_responses/redirect.go
+++ api/_responses/redirect.go
@@ -1,52 +1,17 @@
 package _responses
 
 import (
-	"crypto/rand"
-	"crypto/rsa"
-	"crypto/x509"
-	"encoding/pem"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
 	"net/url"
-	"os"
+	"strconv"
 	"time"
 
-	"github.com/go-jose/go-jose/v4"
-	"github.com/go-jose/go-jose/v4/jwt"
 	"github.com/t2bot/matrix-media-repo/api/_apimeta"
 	"github.com/t2bot/matrix-media-repo/common/rcontext"
 )
-
-// TODO: Persist and store key (or use some other source of information)
-var jwtKey, keyErr = rsa.GenerateKey(rand.Reader, 2048)
-var jwtSig, jwtErr = jose.NewSigner(jose.SigningKey{Algorithm: jose.HS256, Key: []byte("0102030405060708090A0B0C0D0E0F10")}, (&jose.SignerOptions{}).WithType("JWT"))
-var jweEnc, jweErr = jose.NewEncrypter(jose.A128GCM, jose.Recipient{Algorithm: jose.RSA_OAEP, Key: &jwtKey.PublicKey}, nil)
-
-func init() {
-	// We don't expect these to happen, so just panic
-	if keyErr != nil {
-		panic(keyErr)
-	}
-	if jwtErr != nil {
-		panic(jwtErr)
-	}
-	if jweErr != nil {
-		panic(jweErr)
-	}
-
-	f1, _ := os.Create("./gdpr-data/jwe.rsa")
-	f2, _ := os.Create("./gdpr-data/jwe.rsa.pub")
-	defer f1.Close()
-	defer f2.Close()
-	keyPem := pem.EncodeToMemory(&pem.Block{
-		Type:  "RSA PRIVATE KEY",
-		Bytes: x509.MarshalPKCS1PrivateKey(jwtKey),
-	})
-	pubPem := pem.EncodeToMemory(&pem.Block{
-		Type:  "RSA PUBLIC KEY",
-		Bytes: x509.MarshalPKCS1PublicKey(&jwtKey.PublicKey),
-	})
-	f1.Write(keyPem)
-	f2.Write(pubPem)
-}
 
 type RedirectResponse struct {
 	ToUrl string
@@ -54,38 +19,46 @@ type RedirectResponse struct {
 
 func Redirect(ctx rcontext.RequestContext, toUrl string, auth _apimeta.AuthContext) *RedirectResponse {
 	if auth.IsAuthenticated() {
-		// Figure out who we're authenticating as, and add that as JWT claims
-		claims := jwt.Claims{
-			Issuer: ctx.Request.Host,
-		}
-		moreClaims := struct {
-			Admin       bool   `json:"adm,omitempty"`
-			AccessToken string `json:"tok,omitempty"`
-		}{}
+		// Figure out who is authenticated here, as that affects the expiration time
+		var expirationTime time.Time
 		if auth.Server.ServerName != "" {
-			claims.Subject = auth.Server.ServerName
-
-			// The server won't necessarily re-auth itself with the redirect, so we just put an expiration on it instead
-			claims.Expiry = jwt.NewNumericDate(time.Now().Add(2 * time.Minute))
+			expirationTime = time.Now().Add(time.Minute)
 		} else {
-			claims.Subject = auth.User.UserId
-			moreClaims.Admin = auth.User.IsShared
-			moreClaims.AccessToken = auth.User.AccessToken
-		}
-		raw, err := jwt.Encrypted(jweEnc).Claims(claims).Claims(moreClaims).Serialize()
-		if err != nil {
-			panic(err) // should never happen if we've done things correctly
+			expirationTime = time.Now().Add(time.Minute * 5)
 		}
 
-		// Now add the query string
-		parsedUrl, err := url.Parse(toUrl)
-		if err != nil {
-			panic(err) // it wouldn't have worked anyways
+		// Append the expiration time to the URL
+		toUrl = appendQueryParam(toUrl, "matrix_exp", strconv.FormatInt(expirationTime.UnixMilli(), 10))
+
+		// Prepare our HMAC message contents as a JSON object
+		hmacInput := make(map[string]string)
+		hmacInput["url"] = toUrl
+		if auth.User.UserId != "" {
+			hmacInput["access_token"] = auth.User.AccessToken
 		}
-		qs := parsedUrl.Query()
-		qs.Set("org.matrix.media_auth", raw)
-		parsedUrl.RawQuery = qs.Encode()
-		toUrl = parsedUrl.String()
+		hmacMessage, err := json.Marshal(hmacInput)
+		if err != nil {
+			panic(err) // "should never happen"
+		}
+
+		// Actually do the HMAC
+		mac := hmac.New(sha256.New, []byte("THIS_IS_A_SECRET_KEY")) // TODO: @@ Actual secret key
+		mac.Write(hmacMessage)
+		verifyHmac := mac.Sum(nil)
+
+		// Append the HMAC to the URL
+		toUrl = appendQueryParam(toUrl, "matrix_verify", hex.EncodeToString(verifyHmac))
 	}
 	return &RedirectResponse{ToUrl: toUrl}
+}
+
+func appendQueryParam(toUrl string, key string, val string) string {
+	parsedUrl, err := url.Parse(toUrl)
+	if err != nil {
+		panic(err) // it wouldn't have worked anyways
+	}
+	qs := parsedUrl.Query()
+	qs.Set(key, val)
+	parsedUrl.RawQuery = qs.Encode()
+	return parsedUrl.String()
 }
