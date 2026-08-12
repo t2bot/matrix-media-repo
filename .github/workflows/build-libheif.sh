c3a871aaf54c734711297ffbbd61114ebc6afad8
Andrew Ferrazzutti <andrewf@element.io>
2026-08-12 13:24:05 -0400

    Update version of built libheif to match go.mod

--- .github/workflows/build-libheif.sh
+++ .github/workflows/build-libheif.sh
@@ -4,7 +4,7 @@ sudo apt-get update -y
 sudo apt-get install -y git cmake make pkg-config libx265-dev libde265-dev libjpeg-dev libtool
 git clone https://github.com/strukturag/libheif.git
 cd libheif
-git checkout v1.19.5
+git checkout v1.23.1
 mkdir build
 cd build
 cmake --preset=release ..
