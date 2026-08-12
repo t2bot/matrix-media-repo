889e09a0def24646d8925bae0c42dbaec775fcec
Andrew Ferrazzutti <andrewf@element.io>
2026-08-12 13:11:40 -0400

    CI: apt-get update before install

--- .github/workflows/build-libheif.sh
+++ .github/workflows/build-libheif.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -ex
+sudo apt-get update -y
 sudo apt-get install -y git cmake make pkg-config libx265-dev libde265-dev libjpeg-dev libtool
 git clone https://github.com/strukturag/libheif.git
 cd libheif
